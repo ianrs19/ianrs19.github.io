ef8c3a2339dc6e93b98cc5920e16900a48dbeb6f
ianrs_19 <gaborojass33@gmail.com>
2023-06-20 18:22:21 -0600

    Update animations.js

--- js/animations.js
+++ js/animations.js
@@ -72,9 +72,9 @@ economico.style.display = 'none';
 
 const changes1 = () => {
   social.style.display = 'none';
-  cc2.classList.toggle(spanActive);
+  cc2.classList.toggle('spanActive');
   economico.style.display = 'none';
-  cc3.classList.toggle(spanActive);
+  cc3.classList.toggle('spanActive');
   ambiente.style.display = 'flex';
   ambiente.classList.add = 'spanActive';
 }
@@ -82,8 +82,8 @@ const changes1 = () => {
 const changes2 = () => {
   social.style.display = 'flex';
   cc2.classList.add = 'spanActive';
-  cc3.classList.toggle(spanActive);
-  cc1.classList.toggle(spanActive);
+  cc3.classList.toggle('spanActive');
+  cc1.classList.toggle('spanActive');
   economico.style.display = 'none';
   ambiente.style.display = 'none';
 }
@@ -91,8 +91,8 @@ const changes2 = () => {
 const changes3 = () => {
   social.style.display = 'none';
   economico.style.display = 'flex';
-  cc1.classList.toggle(spanActive);
-  cc2.classList.toggle(spanActive);
+  cc1.classList.toggle('spanActive');
+  cc2.classList.toggle('spanActive');
   economico.classList.add = 'spanActive';
   ambiente.style.display = 'none';
 }
